0cd2f7d3cffcace418a0a667258d3a57e9c348fd
jiyong <p_ji_y@naver.com>
2021-03-30 23:58:03 +0900

    prom 슬라이더 추가

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -66,4 +66,33 @@ $(document).ready(function(){
         autoControlsCombine: true,
     });
 
+    var promSlider = $('.bx-slider').bxSlider( {
+        mode: 'horizontal',// 가로 방향 수평 슬라이드
+        speed: 500,        // 이동 속도를 설정
+        pager: false,      // 현재 위치 페이징 표시 여부 설정
+        moveSlides: 1,     // 슬라이드 이동시 개수
+        slideWidth: 1100,   // 슬라이드 너비
+        minSlides: 1,      // 최소 노출 개수
+        maxSlides: 1,      // 최대 노출 개수
+        slideMargin: 1,    // 슬라이드간의 간격
+        auto: true,        // 자동 실행 여부
+        autoHover: false,   // 마우스 호버시 정지 여부
+        controls: false,    // 이전 다음 버튼 노출 여부,
+        autoControls: true,
+        autoControlsCombine: true,
+    });
+
+    $( '.main_PrevBtn' ).on( 'click', function () {
+        promSlider.goToPrevSlide();
+
+    });
+
+
+$( '.main_nextBtn' ).on( 'click', function () {
+        promSlider.goToNextSlide();
+
+    });
+
+
+
 });
